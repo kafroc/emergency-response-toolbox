8aea36524fbd700c8710041876cd18666af71b0c
Kafroc <kafrocyang@gmail.com>
2021-02-23 19:40:08 +0800

    Update GatherInfo.sh

--- GatherInfo.sh
+++ GatherInfo.sh
@@ -1,4 +1,4 @@
-# update 2021.01.11
+# update 2021.02.23
 
 #!/bin/bash
 
@@ -35,41 +35,68 @@ function init_info(){
 
 function cron_info(){
 	echo "Gathering cron info"
+
 	cat /etc/crontab > /tmp/GatherInfo/crontab.log 2>&1
 	cat /etc/anacrontab > /tmp/GatherInfo/anacrontab.log 2>&1
 	crontab -l > /tmp/GatherInfo/crontab_l.log 2>&1
 	
 	cd /etc/cron.d/
-	cat * > /tmp/GatherInfo/etc_cron.d.log 2>&1
+	ls -alt > /tmp/GatherInfo/etc_cron.d.log 2>&1
+	cat * >> /tmp/GatherInfo/etc_cron.d.log 2>&1
+	
 	cd /etc/cron.daily/
-	cat * > /tmp/GatherInfo/etc_daily.log 2>&1
+	ls -alt > /tmp/GatherInfo/etc_cron.daily.log 2>&1
+	cat * >> /tmp/GatherInfo/etc_cron.daily.log 2>&1
+	
 	cd /etc/cron.hourly/
-	cat * > /tmp/GatherInfo/etc_hourly.log 2>&1
+	ls -alt > /tmp/GatherInfo/etc_cron.hourly.log 2>&1
+	cat * >> /tmp/GatherInfo/etc_cron.hourly.log 2>&1
+	
 	cd /etc/cron.monthly/
-	cat * > /tmp/GatherInfo/etc_monthly.log 2>&1
+	ls -alt > /tmp/GatherInfo/etc_cron.monthly.log 2>&1
+	cat * >> /tmp/GatherInfo/etc_cron.monthly.log 2>&1
+	
 	cd /etc/cron.weekly/
-	cat * > /tmp/GatherInfo/etc_weekly.log 2>&1
+	ls -alt > /tmp/GatherInfo/etc_cron.weekly.log 2>&1
+	cat * >> /tmp/GatherInfo/etc_cron.weekly.log 2>&1
+	
 	cd /var/spool/cron/
-	cat * > /tmp/GatherInfo/var_spool_cron.log 2>&1
+	ls -alt > /tmp/GatherInfo/var_spool_cron.log 2>&1
+	cat * >> /tmp/GatherInfo/var_spool_cron.log 2>&1
+	
 	cd /var/spool/anacron/
-	cat * > /tmp/GatherInfo/var_spool_anacron.log 2>&1
+	ls -alt > /tmp/GatherInfo/var_spool_anacron.log 2>&1
+	cat * >> /tmp/GatherInfo/var_spool_anacron.log 2>&1
 }
 
 function other_info(){
 	echo "Gathering other info"
+	
+	# check system users
 	cat /etc/passwd | grep -v nologin > /tmp/GatherInfo/passwd.log 2>&1
+
+	# check dirs with 777 permition
 	ls -alt /tmp > /tmp/GatherInfo/tmp.log 2>&1
 	ls -alt /var/tmp > /tmp/GatherInfo/var_tmp.log 2>&1
 	ls -alt /dev/shm > /tmp/GatherInfo/dev_shm.log 2>&1
+
+	# check ld env
 	echo $LD_PRELOAD > /tmp/GatherInfo/LD_PRELOAD.log 2>&1
 	cat /etc/ld.so.preload > /tmp/GatherInfo/etc_ld.so.preload.log 2>&1
+
+	# check root ssh config
 	ls -alt /root/.ssh > /tmp/GatherInfo/ls_alt_root_.ssh.log 2>&1
 	cat /root/.ssh/* > /tmp/GatherInfo/cat_root_.ssh.log 2>&1
+
+	# check bash config
+	cat /root/.bash_profile > /tmp/GatherInfo/cat_root_bash_profile.log 2>&1
+	cat /root/.bashrc > /tmp/GatherInfo/cat_root_bashrc.log 2>&1
 	
 	for user in /home/*
 	do
 		if test -d $user;then
-			cat /$user/.ssh/* > /tmp/GatherInfo/cat_$user_.ssh.log 2>&1
+			cat /home/$user/.ssh/* > /tmp/GatherInfo/cat_$user.ssh.log 2>&1
+			ls -alt /home/$user > /tmp/GatherInfo/cat_$user.home.log 2>&1
 		fi
 	done
 }
@@ -84,3 +111,4 @@ other_info
 
 cd /tmp
 tar -zcvf GatherInfo.tar.gz GatherInfo
+
